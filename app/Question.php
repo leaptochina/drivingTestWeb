6e7035a474b0db0023c8c031ba354685d289d1c7
leaptochina <pine.linhai@gmail.com>
2019-11-01 21:35:44 +1300

    修复非vip问题搜索

--- app/Question.php
+++ app/Question.php
@@ -8,5 +8,9 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Question extends Model
 {
     use SoftDeletes;
- 
+
+    public function questionList()
+    {
+        return $this->belongsTo('App\QuestionList');
+    }
 }
